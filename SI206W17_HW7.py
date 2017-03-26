650f59d363d7a11d12d186eda149cc753a03ceca
Alpana Toteja <atoteja@Alpanas-MacBook-Pro-3.local>
2017-03-26 02:17:30 -0400

    second commit

--- SI206W17_HW7.py
+++ SI206W17_HW7.py
@@ -159,19 +159,27 @@ conn.commit()
 
 # Select from the database all of the TIMES the tweets you collected were posted and fetch all the tuples that contain them in to the variable tweet_posted_times.
 
+query = 'SELECT time_posted FROM Tweets'
+tweet_posted_times = list(cur.execute(query))
 
 # Select all of the tweets (the full rows/tuples of information) that have been retweeted MORE than 2 times, and fetch them into the variable more_than_2_rts.
 
-
+query = 'SELECT * FROM Tweets WHERE retweets > 2'
+more_than_2_rts = list(cur.execute(query))
 
 # Select all of the TEXT values of the tweets that are retweets of another account (i.e. have "RT" at the beginning of the tweet text). Save the FIRST ONE from that group of text values in the variable first_rt. Note that first_rt should contain a single string value, not a tuple.
+
+query = 'SELECT tweet_text FROM Tweets WHERE tweet_text LIKE "RT%"'
+x = list(cur.execute(query))
+unpacked = [item[0] for item in x]
+first_rt = unpacked[0]
 
 
 
 # Finally, done with database stuff for a bit: write a line of code to close the cursor to the database.
 
 
-
+conn.close()
 ## [PART 3] - Processing data
 
 # Define a function get_twitter_users that accepts a string as in put and returns a SET of the _twitter screennames_ of each twitter user who was mentioned in that string. 
